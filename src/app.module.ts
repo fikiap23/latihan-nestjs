641904fa39c433175b047f64f06167ced705e400
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-15 23:33:52 +0700

    delete tes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TesModule } from './tes/tes.module';
+
 import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TesModule, TaskModule],
+  imports: [TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tes/tes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TesController } from './tes.controller';
-import { TesService } from './tes.service';
-
-@Module({
-  controllers: [TesController],
-  providers: [TesService],
-})
-export class TesModule {}
