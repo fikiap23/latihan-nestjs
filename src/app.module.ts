b2882000e1735625595a33534be190370f141aeb
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-16 09:49:52 +0700

    connet orm prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { TaskModule } from './task/task.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
