3c9db5e385e2fb480d5c306a4996eb4c66a4bed5
fikiaprian23 <fikiaprian23@gmail.com>
2024-02-16 23:50:51 +0700

    add static rootPath

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [TaskModule, PrismaModule, AuthModule],
+  imports: [
+    TaskModule,
+    PrismaModule,
+    AuthModule,
+    ServeStaticModule.forRoot({ rootPath: 'public' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
